45cc6b47837ab2a0dc37535be7858471b9012b3b
GabsGullo <84150651+GabsGullo@users.noreply.github.com>
2023-11-15 19:03:14 -0300

    Delete src directory

--- src/main/java/com/feverdunk/site/exceptions/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.feverdunk.site.exceptions;
-
-import org.springframework.security.access.AccessDeniedException;
-
-public class AuthorizationException extends AccessDeniedException {
-    public AuthorizationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/feverdunk/site/repository/DesempenhoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.feverdunk.site.repository;
-
-import com.feverdunk.site.models.Desempenho;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DesempenhoRepository extends MongoRepository<Desempenho, Long> {
-}
